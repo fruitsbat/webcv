9cbe9bf122755f14bfed2be4a1c3f1b9bc00232f
bat <zoe@kittycat.homes>
2024-02-25 22:12:30 +0100

    fix typo

--- locales/de.ts
+++ locales/de.ts
@@ -1,7 +1,7 @@
 export default {
   "about.me.heading": "Hallo, ich bin Zoe",
   "about.me.description":
-    "Ich Studiere Software-System-Entwicklung an der Universität Hamburg.",
+    "Ich studiere Software-System-Entwicklung an der Universität Hamburg.",
   skills: "Kenntnisse",
   projects: "Projekte",
 } as const;

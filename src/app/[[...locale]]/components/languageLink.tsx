824215fbe1a2e2fb0245abc7db2fe4f63b99f384
bat <zoe@kittycat.homes>
2024-02-28 21:50:53 +0100

    meow

--- src/app/[[...locale]]/components/languageLink.tsx
+++ src/app/[[...locale]]/components/languageLink.tsx
@@ -1,20 +1,19 @@
 import Link from "next/link";
-import { useParams } from "next/navigation";
-import { FaGlobeEurope } from "react-icons/fa";
+import { FaLanguage } from "react-icons/fa";
 
 export default function LanguageLink({ locale }: { locale: string }) {
   // find locale
   if (locale === "en") {
     return (
       <Link href="/de" className="btn btn-ghost flex items-center gap-2">
-        <FaGlobeEurope />
+        <FaLanguage className="w-6 h-6"/>
         <span>deutsch</span>
       </Link>
     );
   }
   return (
     <Link href="/en" className="btn btn-ghost flex items-center gap-2">
-      <FaGlobeEurope />
+      <FaLanguage className="w-6 h-6" />
       <span>english</span>
     </Link>
   );
--- src/app/[[...locale]]/components/top.tsx
+++ src/app/[[...locale]]/components/top.tsx
@@ -9,7 +9,7 @@ export default function Top() {
 
   return (
     <div id="top" className="flex h-screen w-full snap-start flex-col">
-      <div className="flex flex-row justify-end bg-gradient-to-t from-transparent to-base-200 p-4 px-2">
+      <div className="flex flex-row justify-end bg-gradient-to-t from-transparent to-base-300 p-4 px-2">
         <LanguageLink locale={locale} />
       </div>
       <div className="flex flex-1 flex-grow flex-col items-center justify-center">
